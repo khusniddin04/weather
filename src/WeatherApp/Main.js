8c2732498b0bb85ff3c7ba7f748f61f5a899fc9a
khusniddin04 <khusniddinkamoliddinov6@gmail.com>
2024-02-29 21:28:39 +0500

    khusniddin

---
+++ src/WeatherApp/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./style.css"
+import TopNav from './TopNav'
+import Application from './Application'
+
+
+const Main = () => {
+  return (
+    <div>
+        <TopNav/>
+        <Application/>
+    </div>
+  )
+}
+
+export default Main
---
+++ src/WeatherApp/TopNav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function TopNav() {
+  return (
+    <>
+        <div className='topNav'>
+            <h2>Husniddin | Weather App</h2>
+        </div>
+    </>
+  )
+}
+
+export default TopNav
